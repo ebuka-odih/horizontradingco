097cf91dede142c08edc2c85a03b7bf92871329b
Gnosis <emmagab38@gmail.com>
2023-04-19 15:50:22 +0100

    commit changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,8 @@
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <h3 style="color: white; font-size: 40px; font-weight: bolder">{{ env('APP_NAME') }}</h3>
+{{--                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />--}}
                 </a>
             </div>
 
