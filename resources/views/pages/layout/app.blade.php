46c2a003a2d6ea4d4e2d9cd9ce92b3294b99e028
Gnosis <emmagab38@gmail.com>
2023-04-24 23:08:24 +0100

    commit changes

--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -148,7 +148,7 @@
             }
         </script>
 
-        <script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     </center>
 </div>
 
